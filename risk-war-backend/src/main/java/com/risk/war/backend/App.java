6d099395d7fb0aed454cf0f630322735f83ef444
borjius <pilit63@hotmail.com>
2015-02-10 18:31:14 +0100

    Add jetty configuration to run with maven the backend #2

--- risk-war-backend/src/main/java/com/risk/war/backend/App.java
+++ risk-war-backend/src/main/java/com/risk/war/backend/App.java
@@ -1,13 +1,50 @@
 package com.risk.war.backend;
 
+import java.io.File;
+import java.util.concurrent.CountDownLatch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+	
+	private final static Logger log = LoggerFactory.getLogger(App.class);
+	
+	private final static String DEFAULT_CONTEXT = "META-INF/context.xml";
+	
+	private final static String PROPERTIES_FILE_KEY = "properties.file";
+	private final static String DEFAULT_PROPERTIES_FILE = "risk_war_backend.properties";
+	
+	
+    public static void main( String[] args ) {
+    try {
+    	log.info("Application starting...");
+    	App.createContext(DEFAULT_CONTEXT);
+    	System.out.println("Initialization done");
+    } catch (Throwable ex) {
+		log.error("Unhandled exception. Terminating application.", ex);
+	}
+	try {
+		new CountDownLatch(1).await(); // wait until process killed
+	} catch (InterruptedException e) {
+		log.info("Process interrupted");
+	}
+    }
+    
+    public static ClassPathXmlApplicationContext createContext(String contextName) {
+    	if (System.getProperty(PROPERTIES_FILE_KEY) == null) {
+			System.setProperty(PROPERTIES_FILE_KEY, DEFAULT_PROPERTIES_FILE);
+		}
+		log.info("Classpath: \n{}", System.getProperty("java.class.path").replace(File.pathSeparatorChar, '\n'));
+		log.info("Loading context: {}", contextName);
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(contextName);
+		context.registerShutdownHook();
+		return context;
     }
 }
