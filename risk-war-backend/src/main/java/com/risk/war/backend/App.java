631cda05227a68d36c153fe71e3150597b1096c6
borjius <pilit63@hotmail.com>
2015-02-12 17:47:25 +0100

    Jersey server fails when executing from maven #9

--- risk-war-backend/src/main/java/com/risk/war/backend/App.java
+++ risk-war-backend/src/main/java/com/risk/war/backend/App.java
@@ -7,10 +7,16 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 
+import org.simpleframework.http.core.Container;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
 
+import com.risk.war.backend.service.LoginServiceImpl;
+import com.sun.jersey.api.container.filter.GZIPContentEncodingFilter;
+import com.sun.jersey.api.core.DefaultResourceConfig;
+import com.sun.jersey.api.core.PackagesResourceConfig;
 import com.sun.jersey.simple.container.SimpleServerFactory;
 
 /**
@@ -25,6 +31,7 @@ public class App {
 
 	private final static String PROPERTIES_FILE_KEY = "properties.file";
 	private final static String DEFAULT_PROPERTIES_FILE = "risk_war_backend.properties";
+	private final static String REST_SERVICES_PACKAGE = "com.risk.war.backend.service";
 
 	public static void main(String[] args) {
 		try {
@@ -52,6 +59,7 @@ public class App {
 		log.info("Loading context: {}", contextName);
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				contextName);
+		
 
 		context.registerShutdownHook();
 		return context;
@@ -59,9 +67,10 @@ public class App {
 
 	private static void createJaxServer() throws IllegalArgumentException,
 			IOException {
+		PackagesResourceConfig resourceConfig = new PackagesResourceConfig(REST_SERVICES_PACKAGE);
 		Closeable server = null;
 		try {
-			server = SimpleServerFactory.create("http://localhost:5555");
+			server = SimpleServerFactory.create("http://localhost:5555", resourceConfig);
 			log.info("Server started on port 5555...");
 			System.in.read();
 		} catch (Exception e) {
