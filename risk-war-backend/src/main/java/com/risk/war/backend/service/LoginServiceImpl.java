a0fbf6ffdaebd349603725c3688b686e24f4770c
borjius <pilit63@hotmail.com>
2015-02-12 18:44:37 +0100

    Modify rest services inputstream to mapped object #8

--- risk-war-backend/src/main/java/com/risk/war/backend/service/LoginServiceImpl.java
+++ risk-war-backend/src/main/java/com/risk/war/backend/service/LoginServiceImpl.java
@@ -1,21 +1,14 @@
 package com.risk.war.backend.service;
 
-import java.io.IOException;
-import java.io.InputStream;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.risk.war.backend.Utils;
 import com.risk.war.backend.bean.LoginBean;
 
 @Path("/login")
@@ -26,9 +19,7 @@ public class LoginServiceImpl{
 	
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.TEXT_HTML)
-	public void login(InputStream data) throws JsonParseException, JsonMappingException, IOException {
-		LoginBean loginBean = Utils.mapLoginBean(data);
+	public void login(LoginBean loginBean) {
 		log.info("Trying to log with username: " + loginBean.getUsername());
 	}
 
