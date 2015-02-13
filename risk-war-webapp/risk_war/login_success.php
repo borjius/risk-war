ca91ea3e3612164f3b00d8441588f4bba98f4a6f
borjius <pilit63@hotmail.com>
2015-02-13 14:22:50 +0100

    First demo of map included in php page #6

--- risk-war-webapp/risk_war/login_success.php
+++ risk-war-webapp/risk_war/login_success.php
@@ -6,7 +6,22 @@ header("location:index.php");
 ?>
 
 <html>
+<head><title>RISK WARS :: MAIN MENU</title></head>
+<script src="http://maps.googleapis.com/maps/api/js"></script>
+<script>
+function initialize() {
+  var mapProp = {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:7,
+    mapTypeId:google.maps.MapTypeId.TERRAIN
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+</script>
 <body>
 Login Successful
+<div id="googleMap" style="width:700px;height:600px;"></div>
 </body>
+</html>
 </html>
